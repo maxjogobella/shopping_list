8c284c5e428a8c3d935951799a8e692a63e22e2b
maxjogobella <maxsv_it@tuta.io>
2024-02-03 22:52:50 +0300

    update RepositoryImpl not for database

--- app/src/main/java/com/example/shopping_list/domain/ShopListRepository.kt
+++ app/src/main/java/com/example/shopping_list/domain/ShopListRepository.kt
@@ -1,7 +1,7 @@
 package com.example.shopping_list.domain
 
 interface ShopListRepository {
-    fun addShopList(shopItem: ShopItem)
+    fun addShopItem(shopItem: ShopItem)
     fun editShopList(shopItem: ShopItem)
     fun getShopItem(shopItemId : Int) : ShopItem
     fun getShopList() : List<ShopItem>
