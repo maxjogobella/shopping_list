10407932e7454dafd056619474428c1ca4678bd2
maxjogobella <maxsv_it@tuta.io>
2024-02-03 20:41:12 +0300

    create dao and add @Entity, @Database

---
+++ app/src/main/java/com/example/shopping_list/data/local/dao/ShopDao.kt
@@ -0,0 +1,8 @@
+package com.example.shopping_list.data.local.dao
+
+import androidx.room.Dao
+
+@Dao
+interface ShopDao {
+    
+}
--- app/src/main/java/com/example/shopping_list/domain/ShopItem.kt
+++ app/src/main/java/com/example/shopping_list/domain/ShopItem.kt
@@ -1,6 +1,11 @@
 package com.example.shopping_list.domain
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity("shopitem_table")
 data class ShopItem(
+    @PrimaryKey(autoGenerate = true)
     val id : Int,
     val name : String,
     val count : Int,
