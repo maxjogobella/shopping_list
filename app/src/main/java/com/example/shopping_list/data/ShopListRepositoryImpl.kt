f22d071406fc8d254136ba1848de544ebff389d2
Макс <maxsv_it@tuta.io>
2024-02-25 20:59:04 +0300

    add meditationlivedata

--- app/src/main/java/com/example/shopping_list/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shopping_list/data/ShopListRepositoryImpl.kt
@@ -2,7 +2,11 @@ package com.example.shopping_list.data
 
 import android.app.Application
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.map
+import androidx.lifecycle.switchMap
 import com.example.shopping_list.data.local.database.ShopDatabase
+import com.example.shopping_list.data.local.model.ShopItemDb
 import com.example.shopping_list.data.local.model.mapper.ShopListMapper
 import com.example.shopping_list.domain.model.ShopItem
 import com.example.shopping_list.domain.repository.ShopListRepository
@@ -26,12 +30,13 @@ class ShopListRepositoryImpl(
         return mapper.mapDbModelToEntity(dbModel)
     }
 
-    override fun getShopList(): LiveData<List<ShopItem>> {
-        
+    override fun getShopList(): LiveData<List<ShopItem>> = MediatorLiveData<List<ShopItem>>().apply {
+        addSource(shopListDao.getShopList()) {
+            value = mapper.mapListDbModelToListEntity(it)
+        }
     }
 
     override fun removeItem(shopItem: ShopItem) {
         shopListDao.removeItem(shopItem.id)
     }
-
 }
